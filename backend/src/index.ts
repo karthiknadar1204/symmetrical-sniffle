96b61b510a5e309e29e416e0fb35e2d68919dbe3
karthik <karthiknadar1204@gmail.com>
2024-12-02 10:02:34 +0530

    anthropic setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,19 @@
 require("dotenv").config();
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic();
+
+async function main() {
+  const msg = await anthropic.messages.create({
+  model: "claude-3-5-sonnet-20241022",
+  max_tokens: 1000,
+  temperature: 0,
+    messages: [{
+        role: "user",
+        content: "Hello, how are you?"
+    }],
+  });
+  console.log(msg);
+}
+
+main();
